01af7fe819ed648247866449a97dfbc7b907d22c
jachymsmid <jerejere986@gmail.com>
2025-10-01 22:39:00 +0200

    minor changes

--- main.cpp
+++ main.cpp
@@ -3,14 +3,19 @@
 #include <fstream>
 #include <vector>
 
+// impose initial conditions
 void initial_conditions(){}
 
+// impose boundary conditions
 void boundary_conditions(){}
 
+// construct the mesh
 void mesh(){}
 
+// write the data to a file
 void write_data(){}
 
+// numerical schemes
 void lax_friedrichs(){}
 void lax_wendorf(){}
 void upwind(){}
