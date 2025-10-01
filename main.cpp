de2e52a35742c58d41aa840542a1e0a5ac824e3d
jachymsmid <jerejere986@gmail.com>
2025-10-01 22:12:28 +0200

    Added the main file with a basic structure

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+
+void initial_conditions(){}
+
+void boundary_conditions(){}
+
+void mesh(){}
+
+void write_data(){}
+
+void lax_friedrichs(){}
+void lax_wendorf(){}
+void upwind(){}
+
+int main(){
+  return 0;
+}
