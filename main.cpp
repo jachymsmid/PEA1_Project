2c36d08fb1ba560989de53036c6cf43c750ec89a
Tomáš Heblt <root@DellInspiron.localdomain>
2025-11-10 09:52:09 +0100

    Upwind scheme

--- main.cpp
+++ main.cpp
@@ -319,9 +319,35 @@ struct Lax_Wendroff
 // upwind scheme
 struct Upwind
 {
-  static void func()
-  {
-  }
+    static void solve(Mesh &mesh, SimulationInfo sim_info)
+    {
+        Mesh U_n(mesh);  // uložení stavu z předchozího kroku
+        RealNumber dx = sim_info.step_x;
+        RealNumber dy = sim_info.step_y;
+        RealNumber dt = sim_info.step_t;
+
+        // rychlosti šíření
+        RealNumber a = 1.0f;   // směr proudění ve směru x
+        RealNumber b = 1.0f;   // směr proudění ve směru y
+
+        for (size_t i = 1; i < mesh.getRows() - 1; i++)
+        {
+            for (size_t j = 1; j < mesh.getCols() - 1; j++)
+            {
+                mesh.value_ref(i, j) =
+                    U_n.value(i, j)
+                    - a * (dt / dx) * (U_n.value(i, j) - U_n.value(i - 1, j))
+                    - b * (dt / dy) * (U_n.value(i, j) - U_n.value(i, j - 1));
+            }
+        }
+    }
+
+    static RealNumber cfl(RealNumber dx, RealNumber dy)
+    {
+        RealNumber a = 1.0f, b = 1.0f;
+        //podmínka: dt <= 1 / ( |a|/dx + |b|/dy )
+        return 1.0f / (fabs(a)/dx + fabs(b)/dy);
+    }
 };
 
 // general function to call specific numerical scheme
