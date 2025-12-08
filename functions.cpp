4040d965f98f43089aca022d63ceeb3d383b9d5d
Jáchym Šmíd <jerejere986@gmail.com>
2025-12-08 15:30:49 +0100

    no idea

--- functions.cpp
+++ functions.cpp
@@ -25,33 +25,10 @@ RealNumber Lax_Friedrichs::cfl( RealNumber dx, RealNumber dy )
 // Lax-Wendroff
 
 void Lax_Wendroff::solve(Mesh &mesh, SimulationInfo sim_info)
-{
-  Mesh U_n(mesh);
-  RealNumber lx, ly, dx, dy, dt;
-  dx = sim_info.step_x;
-  dy = sim_info.step_y;
-  dt = sim_info.step_t;
-  lx = dt / dx;
 
-  for(size_t i = 1; i < mesh.getRows() - 1; i++)
-  {
-    for(size_t j = 1; j < mesh.getCols() - 1; j++)
-    {
-      ly = 2*mesh.x(i,j)*dt/dy;
-      mesh.value_ref(i,j) = U_n.value(i,j)
-              - lx / 2 * ( U_n.value(i + 1,j) - U_n.value(i - 1,j) )
-              - ly / 2 * ( U_n.value(i,j + 1) - U_n.value(i,j - 1) )
-              + pow(lx,2.0) / 2 * ( U_n.value(i + 1,j) - 2 * U_n.value(i,j) + U_n.value(i - 1,j) )
-              + pow(ly,2.0) / 2 * (U_n.value(i,j + 1) - 2 * U_n.value(i,j) + U_n.value(i,j - 1) )
-              + ly * lx / 4 * (U_n.value(i + 1,j + 1) - U_n.value(i - 1,j + 1) - U_n.value(i + 1,j - 1) + U_n.value(i - 1,j - 1) );
-    }
-  }
-}
 
 RealNumber Lax_Wendroff::cfl(RealNumber dx, RealNumber dy)
-{
-  return 1.0 / (1.0 / dx + 1.0 / 2.0 * 1.5 * dy);
-}
+
 
 // Upwind
 
